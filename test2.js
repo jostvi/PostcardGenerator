2d839d0f79291f29ef70a078599bdec8f72cd63f
Johan Steen Wingren <steen81@gmail.com>
2019-12-18 17:06:10 +0100

    Har lagt till så pixabay anropas och hämtar en bild på en blomma tillsammans med citatet...jay

--- test2.js
+++ test2.js
@@ -29,15 +29,22 @@ app.listen(port, () => {
 });
 
 app.get('/quote', (req, res) => {
-        request('https://favqs.com/api/qotd', (err, qres, body) => {
-            var json = JSON.parse(body);
-            var quote = json.quote;
-            console.log(body);
-            res.render('response.hjs', {
+        helper.standardCall('https://favqs.com/api/qotd')
+        .then(res1 => { 
+            helper.standardCall('https://pixabay.com/api/?key=14668696-1050eb2ce23d8700022954b86' +
+            '&q=flowers&image_type=photo')
+            .then(res2 => {
+                res.render('response.hjs', {
                 title: 'QuoteGenerator',
-                quote: '"' + quote.body,
-                author: quote.author
-            });
-        });     
-});
+                quote: res1.quote.body,
+                author: res1.quote.author,
+                image: res2.hits[0].webformatURL
+                })
+            })
+            .catch(err => { res.send(err); })
+        })
+        .catch(err => { res.send(err); })
+      
+})
+
 
