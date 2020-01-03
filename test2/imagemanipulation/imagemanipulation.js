6008cfa3998db3dc4d88a5ee1285a8fe7ca4604f
Johan Steen Wingren <steen81@gmail.com>
2020-01-03 09:03:38 +0100

    something

---
+++ test2/imagemanipulation/imagemanipulation.js
@@ -0,0 +1,77 @@
+
+const jimp = require('jimp/*')
+
+let imgRaw = 'imagemanipulation/raw/colmar_3.jpg';
+let imgLogo = 'imagemanipulation/raw/stamp.png';
+
+let imgActive = 'imagemanipulation/active/image.jpg';
+let imgExported = 'imagemanipulation/export/image1.jpg';
+
+
+module.exports = {
+  generate : function(image, quote){
+    console.log('Manipulator: ' + image + '\n' + quote)
+    jimp.read(image)
+    
+      .then(tpl => {
+          tpl 
+              .resize(1024, 768)
+              .quality(60)
+              .greyscale()
+              .brightness(0.3)
+        
+              return tpl
+      })
+  
+      .then(tpl => (tpl.clone().write(imgActive)))
+  
+      .then(() => (jimp.read(imgActive)))
+  
+      /* .then(tpl => (
+          jimp.read(imgLogo).then(logoTpl => {
+            logoTpl
+              .resize(64, 52)
+              .opacity(0.8)
+              .rotate(-25);
+            return tpl.composite(logoTpl, 900, 30, [jimp.BLEND_DESTINATION_OVER, 0.2, 0.2]);
+          })
+        ) */
+  
+      .then(tpl => (
+          jimp.loadFont(jimp.FONT_SANS_64_WHITE)
+          .then(font => ([tpl, font]))
+      ))
+  
+      .then(data => {
+  
+          tpl = data[0];
+          font = data[1];
+          let textData = {
+            text: quote,
+            maxWidth: 800,
+            maxHeight: 500,
+            placementX: 100,
+            placementY: 150
+          }
+
+          return tpl.print(font, textData.placementX, textData.placementY, {
+            text: textData.text,
+            alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
+            alignmentY: jimp.VERTICAL_ALIGN_MIDDLE
+          }, textData.maxWidth, textData.maxHeight);
+        })
+  
+      .then(tpl => (tpl.quality(100).write(imgExported)))
+  
+      .then(tpl => {
+          console.log('exported file: ' + imgExported);
+          return imgExported;
+      })
+  
+      .catch(err => {
+          console.error(err);
+      });
+  }
+}
+
+
---
+++ test2/test2.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const request = require('request');
+const helper = require('../API_helper');
+const pd = require('paralleldots');
+const manipulator = require('./imagemanipulation/imagemanipulation');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'hjs');
+
+app.use((req, res, next) => {
+    console.info(`Got request on ${req.path} (${req.method}).`);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Something');
+});
+
+app.listen(port, () => {
+    console.info(`Server is listening on port ${port}.`);
+    console.info('Available routes are:');
+    app._router.stack.forEach((r) => {
+        if (r.route && r.route.path) console.info(r.route.path);
+    });
+});
+
+app.get('/quote', (req, res) => {
+        helper.standardCall('https://favqs.com/api/qotd')
+        .then(res1 => { 
+            pd.apiKey = 'gSLgAnmknvK9FbxLwV3vIpH9GhSNp74R6CFtvPbLbRw';
+            pd.keywords(res1.quote.body)
+            .then((response) => {
+                console.log(res1.quote.body)
+                console.log(response);
+                let keyword = JSON.parse(response);
+                let sorted = keyword.keywords.sort((a, b) => 
+                Number(b.confidence_score) - Number(a.confidence_score));
+                return sorted[0].keyword;
+            })
+            .catch((error) => {console.log(error)
+        })
+            .then(res2 => { 
+                
+                var url = 'https://pixabay.com/api/?key=14668696-1050eb2ce23d8700022954b86' +
+                '&q=' + res2.split(" ")[0] + '&image_type=photo&orientation=horizontal';
+                console.log(url);
+                helper.standardCall(url)
+        
+                .then(res3 => {
+                    manipulator.generate(res3.hits[0].largeImageURL, res1.quote.body);
+                    console.log(res2);
+                    //console.log(res3);
+                    res.render('response.hjs', {
+                    title: 'QuoteGenerator',
+                    quote: res1.quote.body,
+                    author: res1.quote.author
+                 //   image:  img // res3.hits[0].largeImageURL
+                    })
+                
+                })
+                .catch(err => { res.send(err); })
+        })
+            .catch(err => { res.send(err); })
+        })
+        .catch(err => { res.send(err); })
+      
+})
+
+
