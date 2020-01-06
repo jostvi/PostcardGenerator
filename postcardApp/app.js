7e856776863a88fa150541773d05eb681d362b96
Christina3107 <christina.knepper@gmail.com>
2020-01-06 14:40:51 +0100

    Useless fact route added

--- postcardApp/app.js
+++ postcardApp/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var quoteRouter = require('./routes/quote');
 var galleryRouter = require('./routes/gallery')
+var factRouter = require('./routes/fact')
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/quote', quoteRouter);
 app.use('/gallery', galleryRouter)
+app.use('/fact', factRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ postcardApp/routes/fact.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+
+const pd = require('paralleldots');
+const helper = require('../API_helper');
+const manipulator = require('../imagemanipulation/imagemanipulation');
+
+router.get('/', (req, res) => {
+    helper.standardCall('https://uselessfacts.jsph.pl//random.json?language=en')
+    .then(res1 => { 
+        console.log(res1);
+        pd.apiKey = 'gSLgAnmknvK9FbxLwV3vIpH9GhSNp74R6CFtvPbLbRw';
+        pd.keywords(res1.text)
+        .then((response) => {
+            console.log(res1.text)
+            console.log(response);
+            let keyword = JSON.parse(response);
+            let sorted = keyword.keywords.sort((a, b) => 
+            Number(b.confidence_score) - Number(a.confidence_score));
+            return sorted[0].keyword;
+        })
+        .catch((error) => {console.log(error)
+    })
+        .then(res2 => { 
+            
+            var url = 'https://pixabay.com/api/?key=14668696-1050eb2ce23d8700022954b86' +
+            '&q=' + res2.split(" ")[0] + '&image_type=photo&orientation=horizontal';
+            console.log(res2);
+
+            helper.standardCall(url)
+            .then(res3 => {
+                console.log(res3.hits[0])
+                manipulator.generate(res3.hits[Math.floor(Math.random() * 11)].largeImageURL, res1.text)
+                .then(result => {
+                   //console.log('Fact:' + result);
+                    res.render('fact.hjs', {
+                        title: 'FactGenerator',
+                        fact: res1.text,
+                        url: result
+                        })
+                 })
+            })      
+            .catch(err => { res.send(err); })
+    })
+        .catch(err => { res.send(err); })
+    })
+    .catch(err => { res.send(err); })
+  
+  });
+
+  module.exports = router;
