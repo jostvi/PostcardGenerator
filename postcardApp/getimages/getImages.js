c6b8206526e4e8c5f5070102ed4950949c16e864
Christina3107 <christina.knepper@gmail.com>
2020-01-06 11:20:39 +0100

    Image gallery added

---
+++ postcardApp/getimages/getImages.js
@@ -0,0 +1,33 @@
+const cloudinary = require('cloudinary').v2;
+const urlList = []
+cloudinary.config({ 
+    cloud_name: 'dqhlic2nx', 
+    api_key: '922488389614978', 
+    api_secret: 'V3uoIVVMor9OcTrsxwikdc5VkX4' 
+  });
+
+
+function getImages() {
+    return new Promise ((resolve, reject) => {
+      cloudinary.api.resources( { max_results: 500 },
+        function(error, result) {console.log(error)
+        if (error) {reject('Error')}
+        else 
+        resolve(result)
+      })
+    })};
+  
+  /* getImages()
+  .then((result) => {
+    result.resources.forEach(function(item) {
+      urlList.push(item.url)
+      //console.log(item.url)
+    })
+    console.log(urlList)
+  })
+  .catch((error) => {
+    console.log(error)
+  }); */
+  
+  module.exports.getImages = getImages
+  
