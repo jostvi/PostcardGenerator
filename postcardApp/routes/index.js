f5175af5de63da5182a186920c8e0e62fcc31e50
Christina3107 <christina.knepper@gmail.com>
2020-01-08 15:28:20 +0100

    minor changes to index route

--- postcardApp/routes/index.js
+++ postcardApp/routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const imageGallery = require('../getimages/getImages.js')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PostcardGenerator' });
-});
+router.get('/', (req, res, next) => {
+    
+  imageGallery.getCarouselImages()
+  .then((result) => {
+      return new Promise ((resolve, reject) => {
+        //OBS! bör kanske flyttas till getImages.js
+          let images = { urlList : [] }
+          result.resources.forEach(function(item) {
+              images.urlList.push({url: item.url})
+          
+          })
+          resolve(images)
+      })        
+     
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+    .then(images => {
+        console.log(images);
+        res.render('index', {
+          title: 'Postcard Generator', images
+      })
+    })
+    });
+
 
 module.exports = router;
