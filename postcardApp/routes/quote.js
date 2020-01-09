3c0795bd55c5de23f3b8e1152bfb50b88a2efdc0
Johan Steen Wingren <steen81@gmail.com>
2020-01-09 15:46:42 +0100

    mixtrade med await utan att få det att funka

--- postcardApp/routes/quote.js
+++ postcardApp/routes/quote.js
@@ -5,6 +5,7 @@ const pd = require('paralleldots');
 const helper = require('../API_helper');
 const manipulator = require('../imagemanipulation/imagemanipulation');
 const pixaKey = '14668696-1050eb2ce23d8700022954b86';
+const pixabay = require('../getImages/getpixabayimages.js');
 
 router.get('/', (req, res) => {
     helper.standardCall('https://favqs.com/api/qotd')
@@ -17,38 +18,44 @@ router.get('/', (req, res) => {
                     let keyword = JSON.parse(response);
                     let sorted = keyword.keywords.sort((a, b) =>
                         Number(b.confidence_score) - Number(a.confidence_score));
-                    return sorted[0].keyword;
+                    return sorted;
                 })
                 .catch((error) => {
                     console.log(error)
                 })
-                .then(res2 => {
+                .then(sorted => {
+                    console.log(sorted)
+                    let result = pixabay.getImagesByKeyword(sorted, res)
+                    .then((result) => {
+                        console.log("ute igen...")
+                        console.log(result)
+                        res.send(result);
+                    })
 
-                    var url = 'https://pixabay.com/api/?key=' + pixaKey + '&q=' +
-                        res2.split(" ")[0] + '&image_type=photo&orientation=horizontal';
+                    // var url = 'https://pixabay.com/api/?key=' + pixaKey + '&q=' +
+                    //     res2.split(" ")[0] + '&image_type=photo&orientation=horizontal';
                     // console.log(res2);
-
-                    helper.standardCall(url)
-                        .then(res3 => {
-                            var roof = 9;
-                            console.log(res3.totalHits);
-                            if (res3.totalHits < roof)
-                                roof = res3totalHits + 1;
-                            manipulator.generate(res3.hits[Math.floor(Math.random() * roof)].largeImageURL, res1.quote.body, "quote")
-                                .then(result => {
-                                    //   console.log('Quote:' + result);
-                                    // res.render('response.hjs', {
-                                    // title: 'QuoteGenerator',
-                                    // quote: res1.quote.body,
-                                    // author: res1.quote.author,
-                                    // url: result
-                                    // })
-                                    res.send({
-                                        url: result
-                                    })
-                                })
-                        })
-                        .catch(err => { res.send(err) })
+                    // helper.standardCall(url)
+                    //     .then(res3 => {
+                    //         var roof = 9;
+                    //         console.log(res3.totalHits);
+                    //         if (res3.totalHits < roof)
+                    //             roof = res3totalHits + 1;
+                    //         manipulator.generate(res3.hits[Math.floor(Math.random() * roof)].largeImageURL, res1.quote.body, "quote")
+                    //             .then(result => {
+                    //                 //   console.log('Quote:' + result);
+                    //                 // res.render('response.hjs', {
+                    //                 // title: 'QuoteGenerator',
+                    //                 // quote: res1.quote.body,
+                    //                 // author: res1.quote.author,
+                    //                 // url: result
+                    //                 // })
+                    //                 res.send({
+                    //                     url: result
+                    //                 })
+                    //             })
+                    //     })
+                    //     .catch(err => { res.send(err) })
                 })
                 .catch(err => { res.send(err) })
         })
