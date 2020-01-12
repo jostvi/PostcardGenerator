97dae4cf06f740a7a3935e01bb8ffb06b0b62142
Christina3107 <christina.knepper@gmail.com>
2020-01-12 15:41:22 +0100

    create route added

--- postcardApp/routes/createPostcard.js
+++ postcardApp/routes/createPostcard.js
@@ -3,6 +3,9 @@ var router = express.Router();
 const manipulator = require('../imagemanipulation/imagemanipulation');
 
 router.post('/', (req, res) => {
+    console.log(req.body)
+    console.log(req)
+    console.log(req.body.url)
     manipulator.generate(req.body.url, req.body.text, req.body.tag)
     .then(result => {
         res.send({
