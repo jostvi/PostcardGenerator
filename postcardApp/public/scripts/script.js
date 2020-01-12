e36059c43a85865d9632390dc6e2080e8c3a5078
iriselsa <45967024+iriselsa@users.noreply.github.com>
2020-01-12 17:21:16 +0100

    minor changes in ui

--- postcardApp/public/scripts/script.js
+++ postcardApp/public/scripts/script.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
         imgList = [];
         postcardUrl = "";
     
-    
 
     $("img.bigImage").on("click", function () {
         var src = $(this).attr("src");
