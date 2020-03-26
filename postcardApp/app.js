f376ba1811838b25f71b805c985e9f375dbfcf40
Johan Steen Wingren <steen81@gmail.com>
2020-03-26 16:25:35 +0100

    removed body-parser dependency

--- postcardApp/app.js
+++ postcardApp/app.js
@@ -1,11 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser')
-var logger = require('morgan');
-var cors = require('cors');
-var indexRouter = require('./routes/index');
-var quoteRouter = require('./routes/quote');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var logger = require('morgan')
+var cors = require('cors')
+var indexRouter = require('./routes/index')
+var quoteRouter = require('./routes/quote')
 var galleryRouter = require('./routes/gallery')
 var factRouter = require('./routes/fact')
 var aboutRouter = require('./routes/about')
@@ -18,19 +17,17 @@ var imageRouter = require('./routes/topsecret/images')
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hjs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended : false }))
-app.use(bodyParser.json())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors())
 
-app.use('/', indexRouter);
-app.use('/quote', quoteRouter);
+app.use('/', indexRouter)
+app.use('/quote', quoteRouter)
 app.use('/gallery', galleryRouter)
 app.use('/fact', factRouter)
 app.use('/api/v1/postcards/gallery', apiGalleryRouter)
@@ -49,11 +46,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500)
+  res.render('error')
 });
 
 module.exports = app;
